388fac6d34b1a393a96d6b04d4a6fc757288eeaa
Minhaj Haider Shah <smhaider1808@gmail.com>
2025-04-18 14:17:48 +0500

    Testing problems fixing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,15 +12,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-        \App\Models\Reviews::factory(20)->create();
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory(10)->create();
+        // \App\Models\Reviews::factory(20)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
 
-        // $this->call(NavlinksSeeder::class);
-        // $this->call(dropdownitemSeeder::class);
+        $this->call(NavlinksSeeder::class);
+        $this->call(dropdownitemSeeder::class);
 
     }
 }
